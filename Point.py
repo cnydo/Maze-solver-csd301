4670f92ff0bf2d4cb0bcf034e38e8842f91cbc4e
An Thành Long <oraclevn8@gmail.com>
2022-11-10 20:49:13 +0700

    change point's attributes access modifiers

--- Point.py
+++ Point.py
@@ -8,6 +8,26 @@ class Point:
             - x (float): The x coordinate of the point
             - y (float): The target variable
         """
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
+        
+    @property
+    def x(self) -> float:
+        """
+        This function returns the x attribute of the object
+        
+        Returns:
+            - float: The x attribute of the object
+        """
+        return self.__x
+    
+    @x.setter
+    def x(self, x: float) -> None:
+        """
+        This function sets the x attribute of the object to the value of the argument
+        
+        Args:
+            - x (float): The new value of the x attribute
+        """
+        self.__x = x
         
