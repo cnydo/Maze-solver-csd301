c635e07b9743be4c5f573ddf5476056d4f563a34
An Thành Long <oraclevn8@gmail.com>
2022-11-10 18:03:26 +0700

    update document of point class

--- Point.py
+++ Point.py
@@ -1,5 +1,13 @@
 class Point:
-    def __init__(self, x, y) -> None:
+    def __init__(self, x: float, y: float) -> None:
+        """
+        This function takes in two arguments, x and y, and sets the x and y attributes of the object to
+        the values of the arguments
+        
+        Args:
+            - x (float): The x coordinate of the point
+            - y (float): The target variable
+        """
         self.x = x
         self.y = y
         
