8d40b387ac84898bb95ac251748badbcf710ccd6
An Thành Long <oraclevn8@gmail.com>
2022-11-10 21:06:49 +0700

    update private access modifiers

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from Window import Window
 from Point import Point
 from Line import Line
+from Cell import Cell
 
 def main():
     win = Window(800, 600)
-    p1 = Point
-    l = Line(Point(123, 50), Point(400, 400))
-    win.draw_line(l, 'black')
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
     
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -1,3 +1,4 @@
+from Line import Line
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
@@ -8,43 +9,51 @@ class Window:
             - height (float): size of the window
         """
         # Creating a new window.
-        self._root = Tk()
+        self.__root = Tk()
         # Setting the title of the window to "Maze solver"
-        self._root.title("Maze solver")
+        self.__root.title("Maze solver")
         # Setting the size of the window.
-        self._root.geometry(f"{width}x{height}")
+        self.__root.geometry(f"{width}x{height}")
         # Making the window not resizable.
-        self._root.resizable(False, False)
+        self.__root.resizable(False, False)
         # A method that is called when the user closes the window.
-        self._root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         # Creating a canvas with the given width and height.
-        self._canvas = Canvas(self._root, width=width, height=height, bg='white')
+        self.__canvas = Canvas(self.__root, width=width, height=height, bg='white')
         # Packing the canvas.
-        self._canvas.pack(fill=BOTH, expand=1)
+        self.__canvas.pack(fill=BOTH, expand=1)
         # Runnig state of the window.
-        self._running = False
+        self.__running = False
         
     def redraw(self):
         """
         It forces the window to redraw itself
         """
-        self._root.update_idletasks()
-        self._root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
         
-    def draw_line(self, line, fill_color="black"):
+    def draw_line(self, line: Line, fill_color="black"):
         """
         It draws a line on the canvas.
         Args:
             - line (Line): A Line object
             - fill_color (str, optional): The color of the line, defaults to black (optional)
         """
-        line.draw(self._canvas, fill_color)
+        line.draw(self.__canvas, fill_color)
         
     def wait_for_close(self):
-        self._running = True
-        while self._running:
+        """
+        The function waits for the user to close the window
+        """
+        self.__running = True
+        # A loop that keeps the window open until the user closes it.
+        while self.__running:
             self.redraw()
         print("window closed...")
         
     def close(self):
-        self._running = False
+        """
+        It sets the value of the variable `__running` to `False`
+        """
+        self.__running = False
+        
